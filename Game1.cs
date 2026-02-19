92e194fb1e1febdebd21596fe8ddaaf9b4d7f554
Kandace C. <149726928+kchuop@users.noreply.github.com>
2026-02-19 16:48:18 -0500

    Update Game1.cs
    bippity boop

--- Game1.cs
+++ Game1.cs
@@ -1,36 +1,43 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using System.Collections.Generic;
 
-namespace Test;
+namespace KListDemo1;
+
+public enum GameState
+{
+    MainMenu,
+    Playing
+}
 
 public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    
-    List<Sprite> sprites;
-    List<Sprite> killList;
 
-    PlayerSprite player;
-    
+    private MainMenu _mainMenu;
+    private GameState _currentState;
 
-    Texture2D texture;
-
+    private List<Sprite> _sprites;
+    private PlayerSprite _player;
 
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+
+        _mainMenu = new MainMenu();
+        _currentState = GameState.MainMenu;
     }
 
     protected override void Initialize()
     {
         _graphics.IsFullScreen = false;
-        _graphics.PreferredBackBufferHeight = 2480;
-        _graphics.PreferredBackBufferWidth = 2480;
+        _graphics.PreferredBackBufferWidth = 1435;
+        _graphics.PreferredBackBufferHeight = 760;
         _graphics.ApplyChanges();
 
         base.Initialize();
@@ -39,57 +46,96 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        sprites = new();
-
-        // TODO: use this.Content to load your game content here
-        Texture2D texture = Content.Load<Texture2D>("Stick-Man");
-        Texture2D badGuyTex = Content.Load<Texture2D>("Stick-Man");
+        _mainMenu.LoadContent(Content, GraphicsDevice);
 
         
-        
-        sprites.Add(new Sprite(badGuyTex, new Vector2(400,400))); 
-        sprites.Add(new Sprite(badGuyTex, new Vector2(600,600))); 
-        sprites.Add(new Sprite(badGuyTex, new Vector2(700,700))); 
-        player = new PlayerSprite(texture, Vector2.Zero);
-        
+        Texture2D playerTexture = Content.Load<Texture2D>("Bunny1");
+        Texture2D enemyTexture = Content.Load<Texture2D>("BadCat");
+
+        _sprites = new List<Sprite>
+        {
+            new Sprite(enemyTexture, new Vector2(419, 450)),
+            new Sprite(enemyTexture, new Vector2(621, 250)),
+            new Sprite(enemyTexture, new Vector2(290, 192))
+        };
+
+        _player = new PlayerSprite(playerTexture, Vector2.Zero);
     }
+
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+            Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+
+        switch (_currentState)
+        {
+            case GameState.MainMenu:
+                _mainMenu.Update(gameTime);
+
+                if (_mainMenu.SelectedOption == MainMenu.MenuOption.Start)
+                {
+                    _currentState = GameState.Playing;
+                }
+                break;
+
+            case GameState.Playing:
+                UpdateGameplay(gameTime);
+                break;
+        }
+
+        base.Update(gameTime);
+    }
+
+    private void UpdateGameplay(GameTime gameTime)
+    {
+        KeyboardState keyboard = Keyboard.GetState();
+        Vector2 move = Vector2.Zero;
+
+        if (keyboard.IsKeyDown(Keys.Left)) move.X -= 5;
+        if (keyboard.IsKeyDown(Keys.Right)) move.X += 5;
+        if (keyboard.IsKeyDown(Keys.Up)) move.Y -= 5;
+        if (keyboard.IsKeyDown(Keys.Down)) move.Y += 5;
+
+        _player.position += move;
+
         List<Sprite> killList = new();
-        foreach (var sprite in sprites)
+        foreach (var sprite in _sprites)
         {
             sprite.Update(gameTime);
-            if (sprite.Rect.Intersects(player.Rect))
+            if (sprite.Rect.Intersects(_player.Rect))
             {
                 killList.Add(sprite);
             }
         }
 
         foreach (var sprite in killList)
-        {
-            sprites.Remove(sprite);
-        }
+            _sprites.Remove(sprite);
 
-        
-        player.Update(gameTime);
-        base.Update(gameTime);
+        _player.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Gainsboro);
 
-        // TODO: Add your drawing code here
         _spriteBatch.Begin();
 
-        foreach (var sprite in sprites)
+        switch (_currentState)
         {
-            sprite.Draw(_spriteBatch);
+            case GameState.MainMenu:
+                _mainMenu.Draw(_spriteBatch);
+                break;
+
+            case GameState.Playing:
+                foreach (var sprite in _sprites)
+                    sprite.Draw(_spriteBatch);
+
+                _player.Draw(_spriteBatch);
+                break;
         }
-        player.Draw(_spriteBatch);
+        
         _spriteBatch.End();
 
         base.Draw(gameTime);
