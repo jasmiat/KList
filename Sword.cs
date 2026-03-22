345923d0612572d9fd52a2f80ba1cf900135c500
Jordan-H115 <jordanhaggas@hotmail.com>
2026-03-22 01:33:39 -0400

    Update Sword.cs

--- Sword.cs
+++ Sword.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -10,20 +11,58 @@ internal class Sword : Weapon
 {
     private int dmg; //can be added after we figure out enemy health and stuff
     private int range = 100;
+    private float attackTime = .2f;
+    private float attackTimer = 0f;
+    
+    
+    private float angleStart = -MathHelper.PiOver4;
+    private float angleEnd = MathHelper.PiOver4;
+    
+    float currentAngle;
+    
+    bool isAttacking = false;
 
     public Sword(Texture2D texture) : base(texture,0.3f)
     {
         
     }
 
-    public override Rectangle? Attack(Vector2 playerPos, Vector2 facing)
+    public override void Update(GameTime gameTime)
     {
-        if (timer < cooldown)
+        base.Update(gameTime);
+        if (!isAttacking) return;
+        
+        float time = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        attackTimer += time;
+
+        float attackProg = attackTimer / attackTime;
+        
+        if(attackProg >= 1f)
         {
-            return null;
+            isAttacking = false;
+            return;
         }
 
-        Vector2 offset = facing * range;
+        currentAngle = MathHelper.Lerp(angleStart, angleEnd, attackProg);
+    }
+
+    public void startAttacking()
+    {
+        if (isAttacking) return;
+        
+        isAttacking = true;
+        attackTimer = 0f;
+    }
+
+    public override Rectangle? Attack(Vector2 playerPos, Vector2 facing)
+    {
+      
+        float rotation = currentAngle + GetBaseRotation(facing);
+
+
+        Vector2 offset = GetOffset(rotation, 40f);
+
+        
         Vector2 attackDir = playerPos + offset;
         
         return new Rectangle((int)attackDir.X,(int)attackDir.Y,range,range);
@@ -31,8 +70,15 @@ internal class Sword : Weapon
 
     public override void Draw(SpriteBatch spriteBatch, Vector2 playerPos,  Vector2 facingDir)
     {
+        float rotation = currentAngle + GetBaseRotation(facingDir);
+        
+        Vector2 offset = GetOffset(rotation, 40f);
 
-        Vector2 drawingPos = playerPos + facingDir * 50;
-        spriteBatch.Draw(texture,drawingPos,Color.White);
+        Vector2 drawingPos = playerPos + offset;
+        
+        spriteBatch.Draw(
+            texture, drawingPos, null, Color.White, rotation, new Vector2(texture.Width / 2f, texture.Height), 1f, SpriteEffects.None, 0f
+        );
+        
     }
 }
