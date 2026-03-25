0937f117a9aef1914f256a6a0055a204c77567d2
Kandace C. <149726928+kchuop@users.noreply.github.com>
2026-03-25 11:57:40 -0400

    Update TankEnemy.cs

--- TankEnemy.cs
+++ TankEnemy.cs
@@ -1,12 +1,10 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+
 namespace KListDemo1;
 
 internal class TankEnemy : Enemy
-
 {
-    private Color color; 
     public TankEnemy(Texture2D Texture, Vector2 startPos) : base(Texture, startPos)
     {
         speed = 20f;
