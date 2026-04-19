950c7600423f1dc6fe58bc4767cd6b58b7761e7e
Jasmine Tran <jtran_2181@email.ric.edu>
2026-04-19 13:35:45 -0400

    Update PlayerInfo.cs

--- PlayerInfo.cs
+++ PlayerInfo.cs
@@ -6,39 +6,50 @@ namespace KListDemo1
     public class PlayerInfo
     {
         private Texture2D _pixel;
+        private Texture2D _swordTexture;
+        private Texture2D _bowTexture;
         private SpriteFont _font;
         private PlayerSprite _player;
 
         private int _screenWidth;
         private int _screenHeight;
 
-        public PlayerInfo(GraphicsDevice graphicsDevice, SpriteFont font, int width, int height, PlayerSprite player)
+        public PlayerInfo(
+            GraphicsDevice graphicsDevice,
+            SpriteFont font,
+            int width,
+            int height,
+            PlayerSprite player,
+            Texture2D swordTexture,
+            Texture2D bowTexture)
         {
             _screenWidth = width;
             _screenHeight = height;
             _font = font;
             _player = player;
 
+            _swordTexture = swordTexture;
+            _bowTexture = bowTexture;
+
             _pixel = new Texture2D(graphicsDevice, 1, 1);
             _pixel.SetData(new[] { Color.White });
-        }
+        }   //plz kill me
 
-        public void Draw(SpriteBatch spriteBatch)
+        public void Draw(SpriteBatch spriteBatch, int health, int maxHealth, WeaponType currentWeapon)
         {
-            //size of dialogue box
-            int hudHeight = _screenHeight / 6;
+            // backgroundd
+            int hudHeight = _screenHeight / 5;
             int hudY = _screenHeight - hudHeight;
 
-            // color background
             spriteBatch.Draw(_pixel,
                 new Rectangle(0, hudY, _screenWidth, hudHeight),
                 Color.Gray * 0.7f);
 
-            // da border
             DrawBorder(spriteBatch, new Rectangle(0, hudY, _screenWidth, hudHeight), 3, Color.Black);
-
+            
             DrawPlayer(spriteBatch, hudY);
-            DrawInventory(spriteBatch, hudY);
+            DrawHealthBar(spriteBatch, health, maxHealth, hudY);
+            DrawWeaponIcon(spriteBatch, currentWeapon, hudY);
         }
 
         private void DrawPlayer(SpriteBatch spriteBatch, int hudY)
@@ -47,47 +58,58 @@ namespace KListDemo1
             int iconX = 20;
             int iconY = hudY + 20;
 
-            // KC modified - bunny char animation
-            Rectangle firstFrame = new Rectangle(0, 0, _player.FrameWidth, _player.FrameHeight);
-            spriteBatch.Draw(_player.SpritesheetTexture, new Rectangle(iconX, iconY, iconSize, iconSize), firstFrame, Color.White);
+            spriteBatch.Draw(_player.texture, new Rectangle(iconX, iconY, iconSize, iconSize), Color.White);
             DrawBorder(spriteBatch, new Rectangle(iconX, iconY, iconSize, iconSize), 2, Color.Black);
 
             int textX = iconX + iconSize + 15;
             int textY = iconY + 5;
 
-            spriteBatch.DrawString(_font, "Player", new Vector2(textX, textY), Color.Black);
-
-            // health + mana bar
-            float hp = MathHelper.Clamp(_player.position.X / _screenWidth, 0f, 1f);
-            float mana = MathHelper.Clamp(_player.position.Y / _screenHeight, 0f, 1f);
-
-            DrawBar(spriteBatch, textX, textY + 50, 150, 12, hp, Color.Red);
-            DrawBar(spriteBatch, textX, textY + 100, 150, 12, mana, Color.Blue);
+            spriteBatch.DrawString(_font, "Good Bunny", new Vector2(textX, textY), Color.Black);
         }
 
-        private void DrawInventory(SpriteBatch spriteBatch, int hudY)
+        private void DrawHealthBar(SpriteBatch spriteBatch, int health, int maxHealth, int hudY)
         {
-            int startX = _screenWidth - 260;
-            int startY = hudY + 20;
+            int barWidth = 400;
+            int barHeight = 20;
 
-            int size = 50;
-            int spacing = 10;
+            int x = _screenWidth - 1360;
+            int y = hudY + 80;
 
-            for (int i = 0; i < 4; i++)
+            // da border
+            spriteBatch.Draw(_pixel, new Rectangle(x - 3, y - 3, barWidth + 6, barHeight + 6), Color.Silver);
+
+            // da background
+            spriteBatch.Draw(_pixel, new Rectangle(x, y, barWidth, barHeight), Color.Black);
+
+            float percent = (float)health / maxHealth;
+            int currentWidth = (int)(barWidth * percent);
+
+            // health color
+            spriteBatch.Draw(_pixel, new Rectangle(x, y, currentWidth, barHeight), Color.Red);
+
+            // hp gasp
+            spriteBatch.DrawString(_font, $"HP: {health}/{maxHealth}", new Vector2(x, y - 1), Color.White);
+        }
+
+        private void DrawWeaponIcon(SpriteBatch spriteBatch, WeaponType currentWeapon, int hudY)
+        {
+            Texture2D texture = currentWeapon == WeaponType.Sword ? _swordTexture : _bowTexture;
+
+            int size = 140;
+            int x = _screenWidth - size - 30;
+            int y = hudY + 20;
+
+            // background
+            spriteBatch.Draw(_pixel, new Rectangle(x - 5, y - 5, size + 10, size + 10), Color.White);
+
+            if (texture != null)
             {
-                Rectangle slot = new Rectangle(startX + i * (size + spacing), startY, size, size);
-
-                spriteBatch.Draw(_pixel, slot, Color.White);
-                DrawBorder(spriteBatch, slot, 2, Color.Black);
+                spriteBatch.Draw(texture, new Rectangle(x, y, size, size), Color.White);
             }
-        }
 
-        private void DrawBar(SpriteBatch spriteBatch, int x, int y, int width, int height, float percent, Color color)
-        {
-            spriteBatch.Draw(_pixel, new Rectangle(x, y, width, height), Color.DarkGray);
-            spriteBatch.Draw(_pixel, new Rectangle(x, y, (int)(width * percent), height), color);
-            DrawBorder(spriteBatch, new Rectangle(x, y, width, height), 1, Color.Black);
+            spriteBatch.DrawString(_font, currentWeapon.ToString(), new Vector2(x - 10, y + size + 5), Color.White);
         }
+        
 
         private void DrawBorder(SpriteBatch spriteBatch, Rectangle rect, int thickness, Color color)
         {
