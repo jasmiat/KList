8a002078b145f096b285d9d219d8fa7d29faf52c
Kandace C. <149726928+kchuop@users.noreply.github.com>
2026-04-23 18:58:48 -0400

    Update Game1.cs
    JORDANS COLLISION ADDED ON

--- Game1.cs
+++ Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using System.Collections.Generic;
+using System.IO;
 using System;
 
 namespace KListDemo1;
@@ -27,7 +28,9 @@ public class Game1 : Game
     private MainMenu _mainMenu;
     private GameState _currentState;
 
-    private List<Enemy> _sprites;
+    private List<Enemy> _enemies;
+
+    private int TILESIZE = 64;
 
     // Player
     private PlayerSprite _player;
@@ -47,6 +50,16 @@ public class Game1 : Game
     
     private Texture2D _carrotTexture;
     private Texture2D _swordTexture;
+
+    private Texture2D textureAtlas;
+    private Texture2D rectangleTexture;
+    
+    // Tilemaps
+    private Dictionary<Vector2, int> map;
+    private Dictionary<Vector2, int> collisions;
+    private List<Rectangle> textureStore;
+    private List<Rectangle> intersections;
+    private List<Rectangle> enemyIntersections;
 
     // Health
     private int health = 500;
@@ -70,8 +83,41 @@ public class Game1 : Game
         _graphics.PreferredBackBufferWidth = 1500;
         _graphics.PreferredBackBufferHeight = 1250;
         _graphics.ApplyChanges();
+        
+        map = LoadMap(Path.Combine(Content.RootDirectory, "output_Tile Layer 1.txt"));
+        collisions = LoadMap(Path.Combine(Content.RootDirectory, "output_Collisions.txt"));
+        
+        intersections = new();
 
         base.Initialize();
+    }
+    
+    private Dictionary<Vector2, int> LoadMap(string filepath)
+    {
+        Dictionary<Vector2, int> result = new();
+
+        StreamReader reader = new(filepath);
+
+
+        int y = 0;
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            string[] items = line.Split(',');
+            for (int x = 0; x < items.Length; x++)
+            {
+                if (int.TryParse(items[x], out int value))
+                {
+                    if (value > -1)
+                    {
+                        result[new Vector2(x, y)] = value;
+                    }
+                }
+            }
+
+            y++;
+        }
+        return result;
     }
 
     protected override void LoadContent()
@@ -82,24 +128,26 @@ public class Game1 : Game
         //player info part
         _font = Content.Load<SpriteFont>("MenuFont");
 
-        Texture2D enemyTexture = Content.Load<Texture2D>("BadCat");
-        Texture2D tankTexture = Content.Load<Texture2D>("BigBadCat");
+        Texture2D enemyTexture = Content.Load<Texture2D>("FarmerEnemy-2");
+        Texture2D tankTexture = Content.Load<Texture2D>("FarmerEnemy-2");
         Texture2D weaponTexture = Content.Load<Texture2D>("weapon");
 
         backgroundTexture = Content.Load<Texture2D>("background");
         deathscreen = Content.Load<Texture2D>("deathscreen");
         
+        textureAtlas = Content.Load<Texture2D>("textureAtlas");
+        
         pixel = new Texture2D(GraphicsDevice, 1, 1);
         pixel.SetData(new[] { Color.White });
 
         // Enemies
-        _sprites = new List<Enemy>
+        _enemies = new List<Enemy>
         
         {
-            new Enemy(enemyTexture, new Vector2(600, 600)),
-            new Enemy(enemyTexture, new Vector2(900, 900)),
-            new Enemy(enemyTexture, new Vector2(1200, 1200)),
-            new TankEnemy(tankTexture, new Vector2(750, 750))
+            new Enemy(enemyTexture, new Vector2(400, 200)),
+            new Enemy(enemyTexture, new Vector2(400, 300)),
+            new Enemy(enemyTexture, new Vector2(400, 400)),
+            new TankEnemy(tankTexture, new Vector2(300, 100))
         };
         
         // Kandace's animation
@@ -159,8 +207,84 @@ public class Game1 : Game
                 break;
         }
 
+        //Player Collisions - Jordan
+        
+        intersections = GetIntersections(_player.Rect);
+        
+        //enemyIntersections = getIntersections(Enemy.Rect);
+        //Left Right Collisions 
+        foreach (var rect in intersections)
+        {
+            if (collisions.TryGetValue(new Vector2(rect.X, rect.Y), out int val))
+            {
+
+                Rectangle collision = new(
+                    rect.X * TILESIZE,
+                    rect.Y * TILESIZE,
+                    TILESIZE,
+                    TILESIZE
+                );
+
+                if (_player.velocity.X > 0.0f)
+                {
+                    _player.position.X = collision.Left - _player.Rect.Width;
+                }else if (_player.velocity.X < 0.0f)
+                {
+                    _player.position.X = collision.Right;
+                }
+
+
+
+            }
+        }
+        
+        //Top Bottom Collisions
+        foreach (var rect in intersections)
+        {
+            if (collisions.TryGetValue(new Vector2(rect.X, rect.Y), out int val))
+            {
+
+                Rectangle collision = new(
+                    rect.X * TILESIZE,
+                    rect.Y * TILESIZE,
+                    TILESIZE,
+                    TILESIZE
+                );
+
+                if (_player.velocity.Y > 0.0f)
+                {
+                    _player.position.Y = collision.Top - _player.Rect.Width;
+                }else if (_player.velocity.Y < 0.0f)
+                {
+                    _player.position.Y = collision.Bottom;
+                }
+            }
+        }
+
         base.Update(gameTime);
     }
+    
+    // Jordan - collisions
+    public List<Rectangle> GetIntersections(Rectangle rect)
+    {
+        List<Rectangle> tiles = new();
+
+        int left = rect.Left / TILESIZE;
+        int right = (rect.Right -1) / TILESIZE;
+        int top = rect.Top / TILESIZE;
+        int bottom = (rect.Bottom - 1) / TILESIZE;
+
+        for (int y = top; y < bottom; y++)
+        {
+            for (int x = left; x < right; x++)
+            {
+                tiles.Add(new Rectangle(x,y,1,1));
+            }
+        }
+
+        return tiles;
+    }
+    
     //Jordan's part - KC modified
     private void UpdateGameplay(GameTime gameTime)
     {
@@ -190,11 +314,75 @@ public class Game1 : Game
 
             hitbox = _weapon.Attack(_player.position, _player.FacingDirection);
         }
+        
+                foreach (var sprite in _enemies)
+        {
+            sprite.Update(gameTime, _player.position);
+            
+
+            sprite.position.X += sprite.velocity.X * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            var tiles = GetIntersections(sprite.Rect);
+
+            foreach (var rect in tiles)
+            {
+                if (collisions.TryGetValue(new Vector2(rect.X, rect.Y), out int val))
+                {
+
+                    Rectangle collision = new(
+                        rect.X * TILESIZE,
+                        rect.Y * TILESIZE,
+                        TILESIZE,
+                        TILESIZE
+                    );
+                    if (sprite.Rect.Intersects(collision))
+                    {
+                        if (sprite.velocity.X > 0.0f)
+                        {
+                            sprite.position.X = collision.Left - sprite.Rect.Width;
+                        }
+                        else if (sprite.velocity.X < 0.0f)
+                        {
+                            sprite.position.X = collision.Right;
+                        }
+
+                        sprite.velocity.X = 0;
+                    }
+                }
+            } 
+            sprite.position.Y += sprite.velocity.Y * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            tiles = GetIntersections(sprite.Rect);
+            foreach (var rect in tiles)
+            {
+                if (collisions.TryGetValue(new Vector2(rect.X, rect.Y), out int val))
+                {
+
+                    Rectangle collision = new(
+                        rect.X * TILESIZE,
+                        rect.Y * TILESIZE,
+                        TILESIZE,
+                        TILESIZE
+                    );
+                    if (sprite.Rect.Intersects(collision))
+                    {
+                        if (sprite.velocity.Y > 0.0f)
+                        {
+                            sprite.position.Y = collision.Top - sprite.Rect.Height;
+                        }
+                        else if (sprite.velocity.Y < 0.0f)
+                        {
+                            sprite.position.Y = collision.Bottom;
+                        }
+                        sprite.velocity.Y = 0;
+                    }
+                }
+            }
+            
+        }
 
         List<Enemy> killList = new();
         
         //Alex's part
-        foreach (var sprite in _sprites)
+        foreach (var sprite in _enemies)
         {
             sprite.Update(gameTime, _player.position);
 
@@ -241,7 +429,7 @@ public class Game1 : Game
         //Alex's part end
         
         foreach (var sprite in killList)
-            _sprites.Remove(sprite);
+            _enemies.Remove(sprite);
 
         _player.Update(gameTime);
         
@@ -260,11 +448,57 @@ public class Game1 : Game
                 _mainMenu.Draw(_spriteBatch);
                 break;
 
-            case GameState.Playing:
+            case GameState.Playing: 
+                
+                int display_tilsize = 64;
+                int num_tiles_per_row = 15;
+                int pxl_tile = 8;
+                foreach (var item in map)
+                {
+                    Rectangle dest = new(
+                        (int)item.Key.X * display_tilsize,
+                        (int)item.Key.Y * display_tilsize,
+                        display_tilsize,
+                        display_tilsize
+                    );
 
+                    int x = item.Value % num_tiles_per_row;
+                    int y = item.Value / num_tiles_per_row;
+
+                    Rectangle source = new(
+                        x * pxl_tile,
+                        y * pxl_tile,
+                        pxl_tile,
+                        pxl_tile
+                    );
+
+                    _spriteBatch.Draw(textureAtlas, dest, source, Color.White);
+                }
+                foreach (var item in collisions)
+                {
+                    Rectangle dest = new(
+                        (int)item.Key.X * display_tilsize,
+                        (int)item.Key.Y * display_tilsize,
+                        display_tilsize,
+                        display_tilsize
+                    );
+
+                    int x = item.Value % num_tiles_per_row;
+                    int y = item.Value % num_tiles_per_row;
+
+                    Rectangle source = new(
+                        x * pxl_tile,
+                        y * pxl_tile,
+                        pxl_tile,
+                        pxl_tile
+                    );
+
+                    _spriteBatch.Draw(textureAtlas, dest, source, Color.White);
+                }
+                
                 // Background draw
-                _spriteBatch.Draw(backgroundTexture, GraphicsDevice.Viewport.Bounds, Color.White);
-                foreach (var sprite in _sprites)
+                // _spriteBatch.Draw(backgroundTexture, GraphicsDevice.Viewport.Bounds, Color.White);
+                foreach (var sprite in _enemies)
                     sprite.Draw(gameTime, _spriteBatch);
                 
                 // KC- took out temp health bar
